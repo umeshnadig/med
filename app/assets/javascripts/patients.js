72866f99de044450b776e7ea590233bc6081fd1f
Umesh Nadig <umeshsr@gmail.com>
2016-06-20 01:54:41 +0530

    set registration date default

--- app/assets/javascripts/patients.js
+++ app/assets/javascripts/patients.js
@@ -1,3 +1,5 @@
 $(function() {
 	$('#patient_registration_on').datepicker({dateFormat:'yy-mm-dd'});
+	if($('#patient_registration_on').datepicker("getDate") == null)
+		$("#patient_registration_on").datepicker("setDate", "-0d"); 
 });
