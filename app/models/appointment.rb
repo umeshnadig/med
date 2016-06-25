9560cb74a12eceb8d5809aefad46e945bafdf8f2
Umesh Nadig <unadig@gmail.com>
2016-06-25 16:40:50 +0530

    creating patient while adding a new appointment

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,13 +1,19 @@
 class Appointment < ActiveRecord::Base
+  before_save :build_patient, :on => :create
   belongs_to :patient
   validates :date, :time, presence: true
-  
+  attr_accessor :patient_name
+
   def patient_name
-    patient.full_name if patient
+    @patient_name || (if patient.present? then patient.full_name end)
   end
-  
-  def patient_name=(name)
-    self.patient = Patient.find_by_name(name) unless name.blank?
+
+  def build_patient
+    if self.is_first
+      puts "first time patient"
+      puts self.patient_name
+      self.patient_id = Patient.create({:first_name => @patient_name}).id;
+    end
   end
-  
+
 end
